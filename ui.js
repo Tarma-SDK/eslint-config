9a32892faec07b2c85e8cb0e47e5a2e163fc8d38
Roy Surjano <rsurjano@outlook.com>
2022-09-11 15:16:46 -0500

    first commit eslint config

---
+++ ui.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  rules: {
+    "react/prop-types": 0,
+  },
+
+  settings: {
+    react: {
+      pragma: "React",
+      fragment: "Fragment",
+      version: "detect",
+    },
+  },
+};
